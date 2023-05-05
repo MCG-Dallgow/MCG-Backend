5976bfbdd0f5cf2fab8dd45104e3219eb4d42b1c
SLH335 <114884726+SLH335@users.noreply.github.com>
2023-05-05 18:53:31 +0200

    Improve security by adding helmet

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const helmet = require('helmet')
+
 const app = express()
 const port = 3000
+
+app.use(helmet())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
