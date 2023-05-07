0095866acdfff2f7b579fca4dac2ae7861d9fe8a
SLH335 <114884726+SLH335@users.noreply.github.com>
2023-05-07 18:54:04 +0200

    Add student class to database on signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,12 @@ const { WebUntis } = require('webuntis');
 // DATABASE
 const connection = require('../services/db')
 
+async function getStudentClass(untis) {
+    const classId = untis.sessionInformation.klasseId
+    const classes = await untis.getClasses()
+    return classes.filter((class_) => class_.id == classId)[0]
+}
+
 exports.signup = async (req, res, next) => {
     // get credentials from request body
     const username = req.body['username']
@@ -11,7 +17,7 @@ exports.signup = async (req, res, next) => {
     // check WebUntis credentials and login
     const untis = new WebUntis('Marie-Curie-Gym', username, password, 'herakles.webuntis.com')
     try {
-        await untis.login()
+        await untis.login() 
     } catch(err) {
         res.status(400).json({message: 'invalid credentials'})
         return;
@@ -22,16 +28,18 @@ exports.signup = async (req, res, next) => {
         if (err) next(err)
         if(data.length > 0) {
             res.status(400).json({message: 'user already exists'})
+            await untis.logout()
             return;
         } else {
             // get student data from WebUntis
             const students = await untis.getStudents()
-            const student = students.filter(student => student.name == username)[0];
+            const student = students.filter(student => student.name == username)[0]
+            const studentClass = await getStudentClass(untis)
             
             // create new user in database
             connection.query(
-                `INSERT INTO user(username, firstname, lastname, password) 
-                VALUES ("${username}", "${student.foreName}", "${student.longName}", "${password}")`
+                `INSERT INTO user(username, firstname, lastname, class) 
+                 VALUES ("${username}", "${student.foreName}", "${student.longName}", "${studentClass.name}")`
             )
 
             res.json({message: 'user created'})
