878b2df9d5268c38b01a8184df9e95b392f3f6e4
SLH335 <114884726+SLH335@users.noreply.github.com>
2023-05-10 16:39:41 +0200

    Use env variables for database host and name

--- services/db.js
+++ services/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'mcg-app'
+    database: process.env.DB_NAME
 })
 
 connection.connect()
