a6332931d503c6ef77d2e76097e3072366b93b3e
SLH335 <114884726+SLH335@users.noreply.github.com>
2023-05-13 22:17:57 +0200

    Remove unneccessary function in db.ts

--- src/services/db.ts
+++ src/services/db.ts
@@ -2,16 +2,4 @@ import { PrismaClient } from '@prisma/client';
 
 const db = new PrismaClient();
 
-async function main() {
-    console.log('Database connected successfully');
-}
-
-main()
-    .catch((e) => {
-        console.error(e.message);
-    })
-    .finally(async () => {
-        await db.$disconnect();
-    });
-
 export default db;
