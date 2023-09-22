0ebe48002902848872e20bc4d5ad47011dee7f75
SLH335 <114884726+SLH335@users.noreply.github.com>
2023-09-22 23:19:52 +0200

    Load posts in descending order

--- src/controllers/news.ts
+++ src/controllers/news.ts
@@ -1,12 +1,12 @@
 import { RequestHandler } from 'express';
-import { eq } from 'drizzle-orm';
 
 import * as auth from './auth';
 import db from '../db/db';
-import { posts, Post } from '../db/schema';
+import { posts } from '../db/schema';
+import { desc } from 'drizzle-orm';
 
 export const getPosts: RequestHandler = async (_, res) => {
-    const result = await db.select().from(posts);
+    const result = await db.select().from(posts).orderBy(desc(posts.creationDate));
         
     res.json({ data: result });
 }
@@ -18,9 +18,9 @@ export const addPost: RequestHandler = async (req, res) => {
 
     const title = req.body['title'] ?? "";
     const data = req.body['data'] ?? "";
-    const now = new Date();
+    const now = new Date(Date.now());
 
-    await db.insert(posts).values({ title: title, authorId: user.id, creationDate: now, editedDate: now, data: data });
+    await db.insert(posts).values({ title: title, authorId: user!.id, creationDate: now, editedDate: now, data: data });
 
     res.json({ message: "ok" });
 }
